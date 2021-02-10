2063bf1d366c5d7a98e7056d7e6805d15fec37f6
ronny761 <ronny@gmail.com>
2021-02-10 08:56:39 +0200

    my solution to thw practice

--- Loop_Sadna24/src/Q4.java
+++ Loop_Sadna24/src/Q4.java
@@ -4,7 +4,9 @@ public class Q4 {
 	public static void main(String[] args) {
 		// solution for question 4 in sadna24
 		System.out.println("hola ronny");
-
+		for (int i = 0; i < 4; i++) {
+			System.out.println("q4 solution");
+		}
 	}
 
 }
---
+++ Loop_Sadna24/src/Q5B.java
@@ -0,0 +1,9 @@
+
+public class Q5B {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
