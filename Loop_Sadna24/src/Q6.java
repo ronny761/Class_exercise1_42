0127c723a9f481d3385781f8ec3f79fa75a1f325
Owner <Owner@DESKTOP-R3OV270>
2021-02-11 17:59:37 +0200

    a

---
+++ Loop_Sadna24/src/Q6.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Q6 {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		for(int i=5; i>0; i--) {
+			System.out.print(i);
+			for(int a=0; a<i; a++)
+				System.out.print("X");
+			System.out.println(" ");
+	}
+}}
