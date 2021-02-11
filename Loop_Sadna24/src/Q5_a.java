c0471a0e10313a06b9eabf36c6f5885e021cc2dd
oriif <oriifergan14@gmail.com>
2021-02-11 12:10:15 +0200

    Hola this is question number 5a

---
+++ Loop_Sadna24/src/Q5_a.java
@@ -0,0 +1,14 @@
+
+public class Q5_a {
+
+	public static void main(String[] args) {
+		for (int count=1; count<6; count++) 
+		{
+		 System.out.print(count); 
+		 for (int i=1; i<=count; i++) 
+			 System.out.print("X"); 
+		 System.out.println("");
+		}
+	}
+
+}
