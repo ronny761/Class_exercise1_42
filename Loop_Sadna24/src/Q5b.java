855d53a04a297cb97d5ba9f8c0173c01085bcbb3
Владимир <@DESKTOP-LTH8G7A>
2021-02-13 14:42:36 +0200

    asiti sadna 24

--- Loop_Sadna24/src/Q5b.java
+++ Loop_Sadna24/src/Q5b.java
@@ -1,15 +1,12 @@
 import java.util.Scanner;
-
-public class Q5A {
+public class Q5b {
 
 	public static void main(String[] args) {
+		
 		for(int i =1; i <= 5; i++) {
-			System.out.print(i);
 			for(int j=1; j <= i; j++) {
-				System.out.print("X");
+				System.out.print(" "+j);
 			}
 			System.out.println();
-}
 	}
-
-}
+}}
---
+++ Loop_Sadna24/src/Q6b.java
@@ -0,0 +1,13 @@
+
+public class Q6b {
+
+	public static void main(String[] args) {
+		for(int i =2; i <= 6; i++) {
+			for(int j=i; j >= 1; j--) {
+				System.out.print(j);
+			}
+			System.out.println();
+
+	}
+	}
+}
